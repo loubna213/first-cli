d32beb2332f270eeb066ed32fcf6b145a74b8014
loubna213 <98254998+loubna213@users.noreply.github.com>
2025-01-23 10:08:07 +0100

    Update command.js

--- src/command.js
+++ src/command.js
@@ -1,4 +1,4 @@
-import yargs from 'yargs'
+import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers'
 import { newNote, findeNote, getAllNotes, removeAllNotes, removeNote } from './notes.js';
 
